91fba45aac7046eeef95bd661a3a33ed7e95db5c
jvitorfarias <joaovitorfarias4@gmail.com>
2023-07-13 21:40:24 -0300

    Algumas correções no código, deixando-o mais limpo e resolvendo o problema das fotos, onde antes aparecia apenas o caminho, agora aparece a foto propriamente dita.

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ fetch("carsList.json").then((response) => {
       header.innerText = cars.brand
       folder.appendChild(header)
 
-      const photos = document.createElement("p")
-      photos.innerHTML = cars.photo
+      const photos = document.createElement("img")
+      photos.src = cars.photo
+      photos.className = "photoCars"
       folder.appendChild(photos)
 
       const dataSheetModel = document.createElement("p")
@@ -36,55 +37,69 @@ fetch("carsList.json").then((response) => {
       })
       folder.appendChild(button)
       cars1.appendChild(folder)
-    });
+    })
   })
-  })
+})
 
-const form = document.getElementById("formFinanciamento");
+const form = document.getElementById("formFinanciamento")
+const tabela = document.getElementById("resultado")
 
 form.addEventListener("submit", function(event) {
   event.preventDefault()
 
-  let valor = Number(document.getElementById("valor").value);
-  //let valorConvertido = Number(valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL"}))
-  let parcelas = Number(document.getElementById("parcelas").value);
-  let resultado = valor / parcelas
-  //let resultadoFormatado = resultado.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
-  document.getElementById("resultado").textContent = `${parcelas} parcelas de R$ ${resultado * 1.7}`
-});
+  let valor = Number(document.getElementById("valor").value)
 
-//const veiculos = ["Carro", "Moto", "Caminhão", "Van", "Ônibus"];
-//  const selectVeiculo = document.getElementById("veiculo");
+  let parcelas = Number(document.getElementById("parcelas").value)
+  let resultado = (valor / parcelas) * 1.7
+  let valorTotal = resultado * parcelas
+
+  tabela.className = "tabela"
+
+  let prestacao = document.createElement("p")
+  prestacao.innerText = `Valor prestação (R$): ${(resultado).toFixed(2)}`
+  tabela.appendChild(prestacao)
+
+  let prestacoes = document.createElement("p")
+  prestacoes.innerText = `Nº prestações: ${parcelas} `
+  tabela.appendChild(prestacoes)
+
+  let financiado = document.createElement("p")
+  financiado.innerText = `Valor financiado (R$): ${(valorTotal).toFixed(2)}`
+  tabela.appendChild(financiado)
+})
+
+//const veiculos = ["Carro", "Moto", "Caminhão", "Van", "Ônibus"]
+//  const selectVeiculo = document.getElementById("veiculo")
 //  
 //  veiculos.forEach(veiculo => {
-//    const option = document.createElement("option");
-//    option.textContent = veiculo;
-//    selectVeiculo.appendChild(option);
-//  });
+//    const option = document.createElement("option")
+//    option.textContent = veiculo
+//    selectVeiculo.appendChild(option)
+//  })
 //
 //  selectVeiculo.addEventListener('change', function() {
-//    const veiculoSelecionado = selectVeiculo.value;
-//    document.getElementById('veiculoSelecionado').textContent = "Veículo selecionado: " + veiculoSelecionado;
-//  });
+//    const veiculoSelecionado = selectVeiculo.value
+//    document.getElementById('veiculoSelecionado').textContent = "Veículo selecionado: " + veiculoSelecionado
+//  })
 //
 //  document.addEventListener('keydown', function(event) {
 //    if (event.keyCode === 13) {
-//      console.log('Tecla Enter pressionada!');
+//      console.log('Tecla Enter pressionada!')
 //    }
-//  });
+//  })
   
-/*const calcularFinanciamento = (valor, juros, parcelas) =>  valor + (valor * juros * parcelas);
+/*const calcularFinanciamento = (valor, juros, parcelas) =>  valor + (valor * juros * parcelas)
 
-let valor = parseFloat(prompt("Digite o valor do financiamento:"));
-let juros = parseFloat(prompt("Digite a taxa de juros (% ao mês):"));
-let parcelas = parseInt(prompt("Digite a quantidade de parcelas:"));
-let resultado = calcularFinanciamento(valor, juros, parcelas);
+let valor = parseFloat(prompt("Digite o valor do financiamento:"))
+let juros = parseFloat(prompt("Digite a taxa de juros (% ao mês):"))
+let parcelas = parseInt(prompt("Digite a quantidade de parcelas:"))
+let resultado = calcularFinanciamento(valor, juros, parcelas)
 
-alert("O valor total do financiamento é: R$ " + resultado);
+alert("O valor total do financiamento é: R$ " + resultado)
 
 let valor = prompt("Insira o valor do financiamento")
 let taxa = prompt("Taxa")
 let parcelas = prompt("Quantidade de parcelas")
-let resultado = parseFloat(valor * taxa * parcelas);
+let resultado = parseFloat(valor * taxa * parcelas)
 
 alert ("Valor total financiado é: R$ " + resultado)*/
